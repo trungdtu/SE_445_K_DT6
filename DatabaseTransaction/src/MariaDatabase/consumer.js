60808e2d484fd4afd4a3e034c450afad5fbb0d58
trungdtu <phantrung9k@gmail.com>
2022-12-08 19:59:17 +0700

    commit

--- DatabaseTransaction/src/MariaDatabase/consumer.js
+++ DatabaseTransaction/src/MariaDatabase/consumer.js
@@ -1,20 +1,33 @@
 const amqplib = require('amqplib');
-
+const maria = require('./db')
 async function receiveQueue () {
-  const conn = await amqplib.connect('amqp://localhost:5672');
+  try {
+    const db = await maria.getConnection()
+    const rabitMQ = await amqplib.connect('amqp://localhost:5672');
 
-  const channel = await conn.createChannel();
+    const date = new Date();
 
-  const queue = 'q2';
+    const channel = await rabitMQ.createChannel();
 
-  await channel.assertQueue(queue, {
-    durable: true,
-  });
-  //reciver 
-  await channel.consume(queue, smg => {
-    console.log('Smg: ', smg.content.toString())
-  }, {
-    noAck: true,
-  })
+
+    // db.query(`INSERT INTO 'dbtransaction'.'khachhang' ('Ho', 'Ten', 'NgaySinh', 'SoDienThoai', 'CMND', 'DiaChi') VALUES ('Phan', 'Trung', '2022-12-08', '0123456789', '012345567', 'DaNang');`);
+
+    const queue = 'KhachHang';
+
+    await channel.assertQueue(queue, {
+      durable: true,
+    });
+    //reciver 
+    await channel.consume(queue, async smg => {
+      // await db.query("INSERT INTO `dbtransaction`.`khachhang` (`Ho`, `Ten`, `NgaySinh`, `SoDienThoai`, `CMND`, `DiaChi`) VALUES ('Phan', 'Trung', '2022-12-08', '0123456789', '012345567', 'DaNang')")
+
+      console.log('KhachHang: ', smg.content.toString())
+      console.log('\n\n')
+    }, {
+      noAck: true,
+    })
+  } catch (err) {
+    console.log('Error: ', err);
+  }
 };
 receiveQueue()
---
+++ DatabaseTransaction/src/MariaDatabase/db.js
@@ -0,0 +1,22 @@
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'root',
+  database: 'dbtransaction'
+});
+
+module.exports={
+  getConnection: function(){
+      return new Promise(function(resolve,reject){
+          pool.getConnection().then(function(connection){
+              resolve(connection);
+          }).catch(function(error){
+              reject(error);
+          });
+      });
+  }
+}
+
+module.exports = pool;
