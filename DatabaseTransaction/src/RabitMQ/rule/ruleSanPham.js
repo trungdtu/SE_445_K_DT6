b680f45fc9a03a363f0dcf74eb42166872faabad
TruongHoang01 <Banhathoang01@gmail.com>
2022-12-16 23:50:57 +0700

    fix-rule

--- DatabaseTransaction/src/RabitMQ/rule/ruleSanPham.js
+++ DatabaseTransaction/src/RabitMQ/rule/ruleSanPham.js
@@ -1,4 +1,40 @@
-
+const data = {
+    TenHang: 'pc',
+    GiaTri: '1000',
+    Note: ''
+}
+function checkNumberInString(value){
+    var format = /^[0-9]+$/;
+    for( i=0; i<value.length;i++){
+        if(!value.match(format)) return false;
+    }
+    return true;
+}
+function checkStringInString(value){
+    var format = /^[a-zA-Z]+$/;
+    for( i=0; i<value.length;i++){
+        if(!value.match(format)) return false;
+    }
+    return true;
+}
+function checkSpecialChar(value){
+    var format = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
+    if( value.match(format) ){
+        return true;
+      }else{
+        return false;
+      }
+}
+function isExistSpecialCharInString(value){
+    for( i=0; i<value.length;i++){
+        if(checkSpecialChar(value[i])) return true;
+    }
+    return false;
+}
+// console.log(checkStringInString('lap'));
+//string.match()
+// var str= 'asdjlj32k()*SA09123!!@#$%^&*(()_+'
+//  console.log(isExistSpecialCharInString('asdlk234l'));
 
 const listAbbreviationName = [
     {
@@ -19,33 +55,47 @@ async function abbreviationName(name){
     }
     return name;
 }
-
+function minMaxOfName(value){
+    if(value.length<2 || value.length>50) return false;
+    else return true;
+}
 function nameProductRule(value){
     return new Promise(async(resolve, reject)=>{
-        if(typeof value === 'string') {
-            resolve(await abbreviationName(value));
+        if(!checkStringInString(value) || isExistSpecialCharInString(value)){
+            reject('TenHang cant exist number or special char');
         }
+        else if(value == ''){
+            reject('TenHang cant be blank');
+        }
+        else if(!minMaxOfName(value)) reject('TenHang must be between 2 and 50 characters');
         else {
-            reject('Wrong format for TenHang, TenHang must be a String');
+            resolve(await abbreviationName(value));
         }
     })
 }
-
+nameProductRule(data.TenHang)
+.then((value)=>{
+    console.log(value);
+})
 function priceProductRule(value){
     return new Promise((resolve,reject)=>{
-        if(Number.isInteger(value)) {
-            return resolve(value);
+        if(!checkNumberInString(value)) {
+            reject('GiaTri cant exist char or special char');
         }
-        else reject('Wrong format for GiaTri, GiaTri must be an Integer');
+        else if(value == '')
+            reject('GiaTri cant be blank');
+        else if(value<1000) 
+            reject('GiaTri must greater than 1000');
+        else resolve(parseInt(value)); 
     })
 }
 
 function noteProductRule(value){
     return new Promise((resolve, reject)=>{
         if(typeof value === 'string') {
-            return resolve(true);
+            resolve(value);
         }
-        else return reject({
+        else reject({
             table: 'SanPham',
             col: 'Note',
             message: 'Wrong format for Note, Note must be a String'
